683c2e898181e8148227f28294bc804b6a01a482
Ryan Kam <ryanplkam@gmail.com>
2021-01-31 19:58:16 -0500

    form focus

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -63,7 +63,6 @@ export default function FormDialog() {
             onChange={handleSummaryChange}
           />
           <TextField
-            autoFocus
             margin="dense"
             id="name"
             label="Details"
